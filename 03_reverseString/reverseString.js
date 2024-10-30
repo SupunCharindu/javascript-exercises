b7405b6d54ce7808a92f19c8cf61cf234489bde1
Supun <126235564+SupunCharindu@users.noreply.github.com>
2024-10-30 08:48:29 +0100

    making function to reverse the word

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const chatArray = string.split('');
+    const reverseArray = chatArray.reverse();
+    const reverseString = reverseArray.join('');
 
+    return reverseString;
+    //return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
